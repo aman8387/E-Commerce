c7f3b9cf6156094ecd8860e6c21bd775843c625a
aman8387 <119442432+aman8387@users.noreply.github.com>
2025-03-25 14:31:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./components/admin-view/layout";
+import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
 import AdminLayout from "./components/admin-view/layout";
